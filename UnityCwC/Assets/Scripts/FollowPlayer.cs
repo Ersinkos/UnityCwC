5e5904f6700b0a30ffeccc2c8fa472d8fd8b9b81
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2023-08-29 20:11:57 +0300

    Unity CwC Unit1
    Car game with player input and obstacles
    Plane game with obstacles

---
+++ UnityCwC/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+    bool isActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        //offset the camera behind the player by adding to the player's position.
+        transform.position = player.transform.position + offset;
+    }
+}
